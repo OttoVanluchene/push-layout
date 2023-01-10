5b128d83f8bf751c78bb2910513de41dcd681cdd
v93938 <ovanluchene@vanmarcke.be>
2023-01-10 16:50:35 +0100

    Reverse the grid order

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,21 @@
 <script>
+    let counter = 1;
     let grid = new Array(8);
     for (let i = 0; i < 8; i++) {
         grid[i] = new Array(8);
+        for (let j = 0; j < 8; j++) {
+            grid[i][j] = counter;
+            counter++;
+        }
     }
 </script>
 
 <div class="grid">
     {#each grid as row, rowIndex}
         {#each row as cell, colIndex}
-            <div data-col="{colIndex+1}" data-row="{rowIndex+1}" class="grid-item"></div>
+            <div class="grid-item" style="order: {(8 * (7 - rowIndex) + (colIndex + 1))}">
+                <span>{cell}</span>
+            </div>
         {/each}
     {/each}
 </div>
@@ -18,6 +25,7 @@
         display: grid;
         grid-template-columns: repeat(8, 1fr);
         grid-template-rows: repeat(8, 1fr);
+        grid-template-rows: reverse;
         grid-gap: 5px;
         width: calc(60px * 8 + 5px * 7);
         height: calc(50px * 8 + 5px * 7);
